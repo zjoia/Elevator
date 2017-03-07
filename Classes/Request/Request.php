7546e084bae37256a501f22d2e3a97fa703face4
zjoia <adam@kaleltechnology.com>
2017-03-07 11:20:57 -0700

    Add Floor Class
    Don’t judge me! Time is out!!
    :(

--- Classes/Request/Request.php
+++ Classes/Request/Request.php
@@ -11,5 +11,81 @@ namespace Kuali;
 
 class Request
 {
+    /*+++++++++++++++++
+    / Member Vars
+    ++++++++++++++++++*/
+
+    //Request Associations
+    private $floor;
+    private $isCalled;
+    private $direction;
+
+    /*+++++++++++++++++
+    / Constructor
+    ++++++++++++++++++*/
+    public function __construct($isCalled, $floor)
+    {
+        $this->isCalled = 0;
+        $addFloor = $this->setFloor($floor);
+        if (!$addFloor)
+        {
+            throw new Exception("Unable to create Request due to no floor");
+        }
+    }
+
+    /*+++++++++++++++++
+    / Stubs | Interfaces
+    ++++++++++++++++++*/
+
+    public function getFloor()
+    {
+        return $this->floor;
+    }
+
+    public function setFloor($floor)
+    {
+        if ($floor == null)
+        {
+            return false;
+        }
+
+        $existingRequest = $floor->getRequest();
+        if ($existingRequest != null && $this != $existingRequest)
+        {
+            return false;
+        }
+
+        $oldFloor = $this->floor;
+
+        $this->floor = $floor;
+
+        $this->floor->setRequest($this);
+
+        if ($oldFloor != null)
+        {
+            $oldFloor->setRequest(null);
+        }
+
+        return true;
+    }
+
+    public function getDirection()
+    {
+        return $this->direction;
+    }
+
+    public function setDirection($direction)
+    {
+        $this->direction = $direction;
+    }
+
+    public function setIsCalled($isCalled)
+    {
+        $this->isCalled = $isCalled;
+    }
+    public function getIsCalled()
+    {
+        return $this->isCalled;
+    }
 
 }
